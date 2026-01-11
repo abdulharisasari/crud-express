3d9d263228176db59338463d8fa1495bc04bb525
haris <abdulharisasari4@gmail.com>
2026-01-11 14:06:38 +0700

    remove

--- .history/src/models/index_20260111134447.js
+++
@@ -1,9 +0,0 @@
-const Sequelize = require('sequelize');
-const sequelize = require('../config/database');
-
-const User = require('./user')(sequelize, Sequelize.DataTypes);
-
-module.exports = {
-  sequelize,
-  User,
-};
